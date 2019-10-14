8f148c6e35a8f92c3396486cd7e4b8d44b5844c9
Victor Heijler <victor.heijler@se.experis.com>
2019-10-14 11:17:30 +0200

    Create react tic tac toe tutorial

---
+++ src/components/Square.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../index.css';
+
+function Square(props) {
+    return (
+        <button className="square" onClick={props.onClick}
+        >
+            {props.value}
+        </button>
+    );
+}
+
+export default Square;
